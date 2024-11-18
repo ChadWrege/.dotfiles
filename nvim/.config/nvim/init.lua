ef972630b93a80a86cf4e73a61a0d1969ecef6c8
Chad Wrege <ChadWrege@Outlook.com>
2024-11-18 15:15:30 -0500

    Stow my files

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,5 @@
+-- 1 tab = 4 spaces
+vim.o.tabstop = 4      -- Number of visual spaces per tab
+vim.o.shiftwidth = 4   -- Number of spaces to use for autoindent
+vim.o.expandtab = true -- Use spaces instead of tabs
+
