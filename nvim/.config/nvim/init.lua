4e8166d49816f230530515c7c10671cc676ab581
ChadWrege <ChadWrege@Outlook.com>
2024-11-22 09:18:21 -0500

    Updated nvim configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,2 @@
--- 1 tab = 4 spaces
-vim.o.tabstop = 4      -- Number of visual spaces per tab
-vim.o.shiftwidth = 4   -- Number of spaces to use for autoindent
-vim.o.expandtab = true -- Use spaces instead of tabs
-
+-- Connects to the lua files under lua/
+require('options')
